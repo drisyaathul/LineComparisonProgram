7d7d12a708c0483c3ef7429217134914b99d192b
drisyaathul <drisyak809@gmail.com>
2023-04-16 19:01:59 +0800

    Added : Length of a line Using Cartesian System

--- src/com/bridgelabz/LineComparisonProgram.java
+++ src/com/bridgelabz/LineComparisonProgram.java
@@ -1,7 +1,25 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class LineComparisonProgram {
     public static void main(String[] args) {
         System.out.println("*** Welcome to LINE COMPARISON COMPUTATION PROGRAM ***");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the value of x1 = ");
+        int x1 = scanner.nextInt();
+        System.out.print("Enter the value of y1 = ");
+        int y1 = scanner.nextInt();
+        System.out.print("Enter the value of x2 = ");
+        int x2 = scanner.nextInt();
+        System.out.print("Enter the value of y2 = ");
+        int y2 = scanner.nextInt();
+        double lengthOfLine;
+
+        lengthOfLine = Math.sqrt(Math.pow((x2-x1),2) + Math.pow((y2-y1),2));
+
+        System.out.println("The Length of a Line = " +lengthOfLine);
+
     }
 }
