c65c0c3f61da859777ee6c78ff3d73b01a3f5931
drisyaathul <drisyak809@gmail.com>
2023-04-16 19:36:56 +0800

    Added: Equality Of Two Lines

--- src/com/bridgelabz/LineComparisonProgram.java
+++ src/com/bridgelabz/LineComparisonProgram.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class LineComparisonProgram {
     public static void main(String[] args) {
         System.out.println("*** Welcome to LINE COMPARISON COMPUTATION PROGRAM ***");
-
+//UC1
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the value of x1 = ");
         int x1 = scanner.nextInt();
@@ -15,11 +15,29 @@ public class LineComparisonProgram {
         int x2 = scanner.nextInt();
         System.out.print("Enter the value of y2 = ");
         int y2 = scanner.nextInt();
-        double lengthOfLine;
+        double lengthOfLine1;
 
-        lengthOfLine = Math.sqrt(Math.pow((x2-x1),2) + Math.pow((y2-y1),2));
+        lengthOfLine1 = Math.sqrt(Math.pow((x2-x1),2) + Math.pow((y2-y1),2));
+        System.out.println("The Length of a Line = " +lengthOfLine1);
+//UC2
+        System.out.print("Enter the value of x1 = ");
+        int x3 = scanner.nextInt();
+        System.out.print("Enter the value of y1 = ");
+        int y3 = scanner.nextInt();
+        System.out.print("Enter the value of x2 = ");
+        int x4 = scanner.nextInt();
+        System.out.print("Enter the value of y2 = ");
+        int y4 = scanner.nextInt();
+        double lengthOfLine2;
+        lengthOfLine2 = Math.sqrt(Math.pow((x4-x3),2) + Math.pow((y4-y3),2));
+        System.out.println("The Length of a Line = " +lengthOfLine2);
 
-        System.out.println("The Length of a Line = " +lengthOfLine);
-
+        String line1 = String.valueOf(lengthOfLine1);
+        String line2 = String.valueOf(lengthOfLine2);
+        boolean result = line1.equals(line2);
+        if (result == true)
+            System.out.println("Two Lines are EQUAL");
+        else
+            System.out.println("Two Lines are NOT Equal");
     }
 }
